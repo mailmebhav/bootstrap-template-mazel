39d87b41376ec7fab6b85c85d564e18c2bde2326
Bhavin Patel <mailmebhav@gmail.com>
2018-07-05 16:23:23 +0530

    fixed nav bar toggler menu hide after click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 50) {
+  if ($(window).scrollTop() >= 50) {
     $('header').css('background-color','rgba(51, 51, 51, 0.90)');
-    } else {
+  } else {
     $('header').css('background-color','transparent');
-    }
-    });
+  }
+});
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
